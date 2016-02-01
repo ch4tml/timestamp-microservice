127a2db2bb1ef77bd4e505e7919a990fa685ce8d
Tom <t.m.lemanski@googlemail.com>
2016-02-01 13:53:49 +0000

    Removed timeserver.js reference file. Created route if user types in invalid timestamp

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 var express = require("express");
 var app = express();
-var ts = require("./timeserver");
 var routes = require("./routes/routes");
 var path = require("path");
 var http = require("http");
@@ -13,9 +12,8 @@ app.set('view engine', 'ejs');
 
 // all environments
 app.set('port', process.env.PORT || 8080);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 app.use(express.favicon());
+// Logs actions and requests when server running
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -26,17 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-/*
-ts(8080, function(date){
-    console.log(date);
-});
-*/
-app.get('/', routes.index);
-app.get("/:time", routes.time);
-/*app.get("/time", function(req, res){ // 1454128530
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end("Ayyy lmao");
-});*/
+
+app.get("/", routes.index); // Home route
+app.get("/:time", routes.time); // Time query route
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/routes.js
+++ routes/routes.js
@@ -1,38 +1,30 @@
 "use strict";
 
-var express = require('express');
-var router = express.Router();
-var http = require("http");
-var url = require("url");
-
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
     res.render('index', { title: 'Timestamp Microservice' });
 };
-
+/*
+ * GET time request and return JSON object
+ */
 exports.time = function(req, res){
-    var url = require("url");
     // Regex checks if query passed in is purely a number
     var re = new RegExp(/^\d+$/);
     
     function convertDateToJSON(timeStamp){
-        if(re.test(timeStamp))
-            date = new Date(timeStamp * 1000);
-        else
-            date = new Date(timeStamp);
+        // If timestamp is already unix then need to multiply by 1000 to convert date to seconds
+        var date = re.test(timeStamp) ? new Date(timeStamp * 1000) : new Date(timeStamp);
         var year = date.getFullYear();
         // Months are zero indexed array
         var month = date.getMonth() + 1;
         var day = date.getDate();
-        
+        // Similar check to above, if a unix timestamp passed this should be displayed to the user, because converting from day, month and year would result in different timestamp
         var unix = re.test(timeStamp)? timeStamp : Date.parse(month + " " +  day  + " " + year)/1000;
-
         return JSON.stringify({"unix": unix, "natural": getMonth(month) + " " + day + ", " + year});
     }
-    
+    // Function to convert month number (from month array) to string value
     function getMonth(monthNum){
         switch (monthNum){
             case 1:
@@ -62,14 +54,16 @@ exports.time = function(req, res){
                 
         }
     }
-    
+    // If all well, writehead 200 with mimetype JSON
     res.writeHead(200, {"Content-Type" : "application/json"});
-    
-    var date = convertDateToJSON(req.params.time);
+    // However, we need to display a particular output when a user enters an invalid query url
+    // An invalid date will be parsed as NaN
+    var checkDate = new Date(req.params.time);
+    // Therefore if a string is entered that IS NOT in the form date, month and year we need to return the below
+    if(!re.test(req.params.time) && isNaN(Date.parse(checkDate)))
+        var json = JSON.stringify({"unix": null, "natural": null});
+    else
+        json = convertDateToJSON(req.params.time);
 
-    console.log(re.test(req.params.time));
-    console.log(re);
-    console.log(req.params.time);
-    console.log(date);
-    res.end(date);
+    res.end(json);
 };
